058c079d1fbaabf826e0fdab3a4e3a150231697c
unknown <nastya_oleinik@list.ru>
2025-09-26 10:42:45 +0500

    PrimeVue

--- src/main.js
+++ src/main.js
@@ -9,27 +9,13 @@ import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 import 'primeicons/primeicons.css'
 
-import 'primeflex/primeflex.css' // Добавьте эту строку
-import './style.css' // Убедитесь, что путь правильный
-
-// Глобальная регистрация компонентов PrimeVue
-import Button from 'primevue/button'
-import Menubar from 'primevue/menubar'
-import InputText from 'primevue/inputtext'
-import DataTable from 'primevue/datatable'
-import Column from 'primevue/column'
+import 'primeflex/primeflex.css'
+import './style.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-// Регистрируем компоненты глобально
-app.component('Button', Button)
-app.component('Menubar', Menubar)
-app.component('InputText', InputText)
-app.component('DataTable', DataTable)
-app.component('Column', Column)
 
 app.use(PrimeVue, {
   theme: {
