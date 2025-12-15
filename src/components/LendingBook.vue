ddc1f32cc51a069418545c66e167151f80ad8e96
unknown <nastya_oleinik@list.ru>
2025-12-15 13:42:42 +0500

    Docker http

--- src/components/LendingBook.vue
+++ src/components/LendingBook.vue
@@ -75,7 +75,7 @@ export default {
         const authStore = useAuthStore()
         const token = authStore.token
 
-        const response = await axios.get('https://www.libcore.ru:8000/api/lending', {
+        const response = await axios.get('http://www.libcore.ru:8001/api/lending', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
